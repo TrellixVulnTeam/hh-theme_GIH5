9d84b8cdbb6457989d1907f955b5c1d86ddf5242
Michael-HP <mrevellin@hula-hoop.fr>
2022-05-06 10:29:19 +0200

    Format code

--- hulahoop/header.php
+++ hulahoop/header.php
@@ -8,11 +8,11 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class('antialiased leading-none'); ?>>
-    <?php wp_body_open(); ?>
+<body <?php body_class( 'antialiased leading-none' ); ?>>
+	<?php wp_body_open(); ?>
