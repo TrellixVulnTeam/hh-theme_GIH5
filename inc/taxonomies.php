e97368147029cd55686df09c858d66ea5c8cc3c6
Michael-HP <mrevellin@hula-hoop.fr>
2022-05-12 10:57:55 +0200

    Update taxonomies registration

--- inc/taxonomies.php
+++ inc/taxonomies.php
@@ -1,28 +1,35 @@
 <?php
 $taxonomies = array(
 	array(
-		'name'  =>  'domain',
-		'post_type' => 'project',
-		'public'    =>  true,
-		'show_ui'   =>  true,
-		'show_in_menu' => true,
+		'name'              => 'public',
+		'post_type'         => 'solution',
+		'public'            => true,
+		'show_ui'           => true,
+		'show_in_menu'      => true,
 		'show_in_nav_menus' => true,
 		'show_admin_column' => true,
-		'args'  =>  array(
-			'label' =>  __('Domaine d\'activités', 'hh'),
-			'hierarchical'  =>  true,
-		)
-	)
+		'args'              => array(
+			'labels'       => array(
+				'name'          => __( 'Type de public', 'hulahoop' ),
+				'singular_name' => __( 'Public', 'hulahoop' ),
+				'add_new'       => __( 'Ajouter un type de public', 'hulahoop' ),
+				'add_new_item'  => __( 'Ajouter un type de public', 'hulahoop' ),
+				'not_found'     => __( 'Aucun type de public trouvé', 'textdomain' ),
+
+			),
+			'hierarchical' => false,
+		),
+	),
 );
 
 /**
  * Register Taxonomies
  */
-if ($taxonomies) :
-	foreach ($taxonomies as $taxonomy) :
-		$taxonomy_name = acf_maybe_get($taxonomy, 'name');
-		$taxonomy_cpt = acf_maybe_get($taxonomy, 'post_type');
-		$taxonomy_args = acf_maybe_get($taxonomy, 'args');
-		register_taxonomy($taxonomy_name, $taxonomy_cpt, $taxonomy_args);
+if ( $taxonomies ) :
+	foreach ( $taxonomies as $taxonomy ) :
+		$taxonomy_name = acf_maybe_get( $taxonomy, 'name' );
+		$taxonomy_cpt  = acf_maybe_get( $taxonomy, 'post_type' );
+		$taxonomy_args = acf_maybe_get( $taxonomy, 'args' );
+		register_taxonomy( $taxonomy_name, $taxonomy_cpt, $taxonomy_args );
 	endforeach;
 endif;
